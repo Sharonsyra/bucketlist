bb2fa0e1ebd6df5e50aeb74d58e9df5eaf92e0d5
Sharonsyra <sharonwaithii@gmail.com>
2017-05-30 18:37:13 +0300

    directory for tests

--- tests/tests.py
+++ tests/tests.py
@@ -4,13 +4,37 @@ import json
 from run import app, db
 from models import User, BucketList, Item
 
+from tests import BaseTestCase
 
 class APITestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.app_context().push()
         self.client = app.test_client()
 
+        db.session.close()
+        db.drop_all()
         db.create_all()
+
+        self.register_details = {
+                    'username': "sharon",
+                    'email': "sharon@sharon.com",
+                    'password': "sharz"
+                    }
+
+        self.login_details = {
+                    'username': "sharon",
+                    'password': "sharz"
+                    }
+
+        self.bucketlist_details = {
+                    "name": "go sky-diving",
+                    "done": False
+                    }
+
+        self.item = {
+                    "name": "take many selfies",
+                    "done": False
+                     }
 
         # ... Tests for register ... #
 
@@ -19,7 +43,7 @@ class APITestCase(unittest.TestCase):
         register = {'username': "sharon",
                     'email': "sharon@sharon.com",
                     'password': "sha"}
-        response = self.client.post("/auth/register", data=json.dumps(register))
+        response = self.client.post("/api/v1.0/auth/register", data=json.dumps(register))
         self.assertEqual(response.status_code, 400)
 
     def test_register_username_format(self):
@@ -27,120 +51,134 @@ class APITestCase(unittest.TestCase):
         register = {'username': "123sharon",
                     'email': "sharon@sharon.com",
                     'password': "sharz"}
-        response = self.client.post("/auth/register", data=json.dumps(register))
+        response = self.client.post("/api/v1.0/auth/register", data=json.dumps(register))
         self.assertEqual(response.status_code, 400)
 
-    def test_register_registers_user(self):
+    def test_create_user(self):
         """Test that register method registers a user successfully!"""
         register = {'username': "sharon",
                     'email': "sharon@sharon.com",
                     'password': "sharz"}
-        response = self.client.post("/auth/register", data=json.dumps(register))
+        response = self.client.post("/api/v1.0/auth/register", data=json.dumps(register))
         self.assertEqual(response.status_code, 201)
 
     def test_register_duplicate_usernames(self):
         """Test that register method rejects duplicate usernames"""
+       
         register = {'username': "sharon",
                     'email': "sharon@sharon.com",
                     'password': "sharz"}
-        response = self.client.post("/auth/register", data=json.dumps(register))
+        response = self.client.post("/api/v1.0/auth/register", data=json.dumps(register))
+        self.assertEqual(response.status_code, 201)
+
+        register_duplicate = {'username': "sharon",
+                    'email': "sharon@sharon.com",
+                    'password': "sharz"}
+        response = self.client.post("/api/v1.0/auth/register", data=json.dumps(register_duplicate))
         self.assertEqual(response.status_code, 409)
 
     # ... Tests for login ... #
 
     def test_login_wrong_credentials(self):
         """Test that login rejects wrong credentials!"""
-        register = {'username': "wanja",
-                    'password': "wanjira"}
-        response = self.client.post("/auth/login", data=json.dumps(register))
+        register = {
+                    'username': "wanja",
+                    'password': "wanjira"
+                    }
+        response = self.client.post("/api/v1.0/auth/login", data=json.dumps(register))
         self.assertEqual(response.status_code, 401)
 
     def test_login_blank_credential(self):
         """Test that login rejects blank username or password"""
         register = {'username': "sharon",
                     'password': ""}
-        response = self.client.post("/auth/login", data=json.dumps(register))
+        response = self.client.post("/api/v1.0/auth/login", data=json.dumps(register))
         self.assertEqual(response.status_code, 400)
 
     def test_login_logs_in_user(self):
         """Test that a user logs in successfully!"""
         register = {'username': "sharon",
                     'password': "sharz"}
-        response = self.client.post("/auth/login", data=json.dumps(register))
+        response = self.client.post("/api/v1.0/auth/login", data=json.dumps(register))
         self.assertEqual(response.status_code, 200)
 
     # ... Tests bucketlist ... #
 
-    def test_bucket_list_create(self):
+    def test_create_bucketlist(self):
         """Test that bucket list creates a bucket list"""
         bucketlist = {"name": "go sky-diving",
-                      "items": "take a selfie",
                       "done": False
                       }
-        response = self.client.post("/bucketlists/", data=json.dumps(bucketlist))
+        response = self.client.post("/api/v1.0/bucketlists/", data=json.dumps(bucketlist))
         self.assertEqual(response.status_code, 201)
 
     def test_bucket_list_duplicates(self):
         """Test that bucket list rejects duplicate bucketlists"""
         bucketlist = {"name": "go sky-diving",
-                      "items": "take a selfie",
                       "done": False
                       }
-        response = self.client.post("/bucketlists/", data=json.dumps(bucketlist))
+        response = self.client.post("/api/v1.0/bucketlists/", data=json.dumps(bucketlist))
+        # self.assertEqual(response.status_code, 201)
+
+        bucketlist1 = {"name": "go sky-diving",
+                      "done": False
+                      }
+        response = self.client.post("/api/v1.0/bucketlists/", data=json.dumps(bucketlist1))
         self.assertEqual(response.status_code, 409)
 
-    def test_bucket_list_view_all(self):
+    def test_view_bucketlists(self):
         """Test that bucket list displays all bucket lists"""
-        bucketlist = {"name": "go sky-diving",
-                      "items": "take a selfie",
-                      "done": False
-                      }
-        response = self.client.post("/bucketlists/", data=json.dumps(bucketlist))
+        response = self.app.get("/api/v1.0/bucketlists/",
+                                headers=self.get_token())
         self.assertEqual(response.status_code, 200)
+        output = json.loads(response.data)
+        output = output["bucketlists"]
+        bucketlist1 = output[0]
+        bucketlist2 = output[1]
+        # Both bucket lists are displayed
+        self.assertEqual(bucketlist1.get("title"), "Knowledge Goals")
+        self.assertEqual(bucketlist2.get("title"), "Adventures")
 
-    def test_single_bucket_list_view_one(self):
+    def test_view_bucketlist(self):
         """Test that single bucket list displays a single bucket list"""
         bucketlist = {"name": "go sky-diving",
-                      "items": "take a selfie",
                       "done": False
                       }
-        response = self.client.post("/bucketlist/", data=json.dumps(bucketlist))
+        response = self.client.get("/api/v1.0/bucketlist/", data=json.dumps(bucketlist))
         self.assertEqual(response.status_code, 200)
 
-    def test_single_bucket_list_update(self):
+    def test_update_bucketlist(self):
         """Test that single bucket list updates the bucket list"""
         bucketlist = {"name": "go water rafting",
-                      "items": "take a selfie",
                       "done": False
                       }
-        response = self.client.post("/bucketlists/<id>", data=json.dumps(bucketlist))
+        response = self.client.put("/api/v1.0/bucketlists/<id>", data=json.dumps(bucketlist))
         self.assertEqual(response.status_code, 200)
 
-    def test_single_bucket_list_delete(self):
+    def test_delete_bucketlist(self):
         """Test that single bucket list deletes the bucket list"""
         bucketlist = {"name": "go sky-diving",
-                      "items": "take a selfie",
                       "done": False
                       }
-        response = self.client.post("/bucketlist/<id>", data=json.dumps(bucketlist))
+        response = self.client.delete("/api/v1.0/bucketlist/<id>", data=json.dumps(bucketlist))
         self.assertEqual(response.status_code, 204)
 
     # ... Tests items ... #
 
-    def test_items_display_all(self):
+    def test_get_items(self):
         """Test items displays all items"""
         item = {"name": "take selfies",
                 "done": False
                 }
-        response = self.client.post("/bucketlists/<id>/items/", data=json.dumps(item))
+        response = self.client.get("/api/v1.0/bucketlists/<id>/items/", data=json.dumps(item))
         self.assertEqual(response.status_code, 200)
 
-    def test_items_create(self):
+    def test_create_item(self):
         """Test items creates new item"""
         item = {"name": "take selfies",
                 "done": False
                 }
-        response = self.client.post("/bucketlists/<id>/items/", data=json.dumps(item))
+        response = self.client.post("/api/v1.0/bucketlists/<id>/items/", data=json.dumps(item))
         self.assertEqual(response.status_code, 201)
 
     def test_items_duplicates(self):
@@ -148,21 +186,23 @@ class APITestCase(unittest.TestCase):
         item = {"name": "take selfies",
                 "done": False
                 }
-        response = self.client.post("/bucketlists/", data=json.dumps(item))
+        response = self.client.post("/api/v1.0/bucketlists/", data=json.dumps(item))
         self.assertEqual(response.status_code, 409)
 
-    def test_item_update(self):
+    def test_update_item(self):
         """Test that item updates item"""
         item = {"name": "take many selfies",
                 "done": False
                 }
-        response = self.client.post("/bucketlists/<id>/items/<item_id>", data=json.dumps(item))
+        response = self.client.put("/api/v1.0/bucketlists/<id>/items/<item_id>", data=json.dumps(item))
         self.assertEqual(response.status_code, 200)
 
-    def test_item_delete(self):
+    def test_delete_item(self):
         """Test that item deletes item"""
-        item = {"name": "take selfies"}
-        response = self.client.post("/bucketlists/<id>/items/<item_id>", data=json.dumps(item))
+        item = {"name": "take selfies",
+                "done": False
+                }
+        response = self.client.delete("/api/v1.0/bucketlists/<id>/items/<item_id>", data=json.dumps(item))
         self.assertEqual(response.status_code, 204)
 
 
